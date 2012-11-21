a93547e77d643f1a8f196d0e220ce689d5cdaf5f
dhs3000 <dennishoersch@web.de>
2012-11-21 21:47:18 +0100

    Commented

--- src/main/java/de/dennishoersch/util/inspection/InspectionHelper.java
+++ src/main/java/de/dennishoersch/util/inspection/InspectionHelper.java
@@ -22,18 +22,53 @@ import java.io.IOException;
 
 import javassist.bytecode.ClassFile;
 
+/**
+ * Helper that is passed to class inspectors when called to inspect a ClassFile.
+ * @author hoersch
+ */
 public interface InspectionHelper {
 
+    /**
+     * Loads a class.
+     *
+     * @param type
+     * @return the class
+     * @throws ClassNotFoundException
+     */
     Class<?> loadClass(ClassFile type) throws ClassNotFoundException;
 
+    /**
+     * Gets a ClassInfo of a named class.
+     *
+     * @param name
+     * @return class info
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     ClassInfo getClassInfo(String name) throws IOException, ClassNotFoundException;
 
+    /**
+     * Converts the given ClassFile to a ClassInfo object.
+     *
+     * @param type
+     * @return a class info object
+     */
+    ClassInfo toClassInfo(ClassFile type);
+
+    /**
+     * Simple wrapper of classes, may be backed by real classes or just
+     * {@link ClassFile}.
+     */
     public interface ClassInfo {
+        /**
+         * @return name of the class
+         */
         public String getName();
 
+        /**
+         * @return name of the super class
+         */
         public String getSuperclass();
     }
-
-    ClassInfo toClassInfo(ClassFile type);
 
 }
